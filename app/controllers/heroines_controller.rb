357f52ab1c6dd35d388e96347ec1fed56b45b995
Will Diep <widiep@gmail.com>
2019-11-27 12:05:13 -0800

    Complete

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,48 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+      @heroine = Heroine.find(params["id"])
+  end
+
+  def new
+      @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    # Check if it's valid first
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroines_path(@heroine)
+    else
+      render "new"
+    end
+
+    # @heroine = Heroine.create(heroine_params)
+    # redirect_to heroines_path(@heroine)
+
+  end
+
+  def edit
+      @heroine = Heroine.find(params["id"]) 
+  end
+
+  def update
+      @heroine = Heroine.find(params["id"])
+      if @heroine.update(heroine_params)
+          redirect_to @heroine
+      else
+          render "edit"
+      end
+  end
+
+  private
+
+  def heroine_params
+      params.require(:heroine).permit(:name, :super_name)
+  end
+
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,47 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params["id"])
+  end
+
+  def new
+      @power = Power.new
+  end
+
+  def create
+      @power = Power.new(power_params)
+      if @power.valid?
+          @power.save
+          redirect_to @power
+      else 
+          render "new"
+      end
+  end
+
+  def edit
+      @power = Power.find(params["id"]) 
+  end
+
+  def update
+      @power = Power.find(params["id"])
+      if @power.update(power_params)
+          redirect_to @power
+      else
+          render "edit"
+      end
+  end
+
+private
+
+def power_params
+    params.require(:power).permit(:name, :description)
+end
+
+
+
+
+
+
 end
