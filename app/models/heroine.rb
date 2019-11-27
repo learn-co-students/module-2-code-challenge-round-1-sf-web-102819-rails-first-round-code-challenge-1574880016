d84c54007c2421a5e57c8c01f46e3a2ac84fbf62
Joseh <joseph161216@gmail.com>
2019-11-27 11:30:57 -0800

    reached minimum for code-challenge

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,6 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+
+  # Make sure no two heroines have the same super name.
+  validates :super_name, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  resources :powers, only: [:index, :show, :new, :create, :edit, :update]
+  resources :heroines, only: [:index, :show, :new, :create, :edit, :update]
 end
+
+# The form should also allow each heroine to be created with only one of the existing powers.
+  #when i didn't create the collection_select I couldn't create my heroine. It would give me a status code of 304 and send me to index page.
+  #is it because i didn't fill out the power_id for the heroine_params??????
