05f3e5a2cda182019ac36d19b68b8facecc308fb
mwong068 <mwong068@gmail.com>
2019-11-27 11:48:20 -0800

    mod 2 code challenge

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,18 @@
 class Heroine < ApplicationRecord
+    belongs_to :power
+    validates :super_name, presence: true, uniqueness: true
+
+    def self.search(search)
+        if search
+            super_power = Power.find_by(name: search)
+            if super_power
+                self.where(power_id: super_power)
+            else
+                @heroines = Heroine.all
+            end
+        else
+            @heroines = Heroine.all
+        end
+    end
+
 end
