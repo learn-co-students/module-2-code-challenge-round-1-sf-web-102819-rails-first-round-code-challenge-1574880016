7dead1e04409d1c29b5f791b2791a364219618fa
Mustafa Muse <umpp101@icloud.com>
2019-11-27 12:03:39 -0800

    Final

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,31 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create 
+    if @heroine = Heroine.new(heroine_params)
+      @heroine.save
+      redirect_to '/heroines'
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id, :search)
+  end
+
 end
